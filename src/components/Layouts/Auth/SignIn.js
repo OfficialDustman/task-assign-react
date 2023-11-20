5425675fb4f25de62a88dabe3a1a175e6885173e
Ogo-Ayorinde Shekinah <ogoayorindeshekinah@gmail.com>
2023-11-20 06:12:13 +0100

    Context API

--- src/components/Layouts/Auth/SignIn.js
+++ src/components/Layouts/Auth/SignIn.js
@@ -1,79 +1,85 @@
-import UIButton from '../../Ui/Button';
-import UIForm from '../../Ui/Form';
-import { Form, InputGroup, Spinner } from 'react-bootstrap';
+import UIButton from "../../Ui/Button";
+import UIForm from "../../Ui/Form";
+import { Form, InputGroup, Spinner } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-// import AuthContext from '../../../store/auth-context';
-import { useState, useEffect, useContext } from 'react';
+import AuthContext from "../../../store/auth-context";
+import { useState, useEffect, useContext } from "react";
 
 function SignIn() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLoaded, setIsLoaded] = useState(true)
-    const [data, setData] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoaded, setIsLoaded] = useState(true);
+  const [data, setData] = useState(null);
 
-    const navigate = useNavigate();
-    
-    function submitHandler(e) {
-        e.preventDefault();
+  const ctx = useContext(AuthContext);
+  ctx.changeUserData("I am a boy");
+  console.log(ctx.userData);
 
-        setIsLoaded(false)
+  const navigate = useNavigate();
 
-        const formData = new FormData();
-        formData.append('email', email);
-        formData.append('password', password);
-    
-        fetch("http://localhost/repos/task-assign/api/user/confirmUser.php", {
-            method: 'POST',
-            body: formData,
-        })
-        .then(response => response.json())
-        .then(data => {
-            setData(data);
-            setIsLoaded(true)
-            console.log(data);
+  function submitHandler(e) {
+    e.preventDefault();
 
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-    }
+    setIsLoaded(false);
 
-    return (
-        <UIForm onSubmit={submitHandler}>
-            <Form.Group className="mb-3">
-                <Form.Label>Enter Your Email</Form.Label>
-                <InputGroup>
-                    <InputGroup.Text>Email</InputGroup.Text>
-                    <Form.Control
-                        type="email"
-                        placeholder="user@email.com"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </InputGroup>
-            </Form.Group>
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("password", password);
 
-            <Form.Group className="mb-3">
-                <Form.Label>Enter Your Password</Form.Label>
-                <InputGroup>
-                    <InputGroup.Text>Password</InputGroup.Text>
-                    <Form.Control
-                        type="password"
-                        placeholder="********"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </InputGroup>
-            </Form.Group>
+    fetch("http://localhost/repos/task-assign/api/user/confirmUser.php", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setIsLoaded(true);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
 
-            <UIButton type='submit'>
-                {isLoaded ? 'Sign In' : 
-                <Spinner animation="border" variant="light" size="sm" />}
-            </UIButton>
-        </UIForm>
-    )
+  return (
+    <UIForm onSubmit={submitHandler}>
+      <Form.Group className="mb-3">
+        <Form.Label>Enter Your Email</Form.Label>
+        <InputGroup>
+          <InputGroup.Text>Email</InputGroup.Text>
+          <Form.Control
+            type="email"
+            placeholder="user@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </InputGroup>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Enter Your Password</Form.Label>
+        <InputGroup>
+          <InputGroup.Text>Password</InputGroup.Text>
+          <Form.Control
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </InputGroup>
+      </Form.Group>
+
+      <UIButton type="submit">
+        {isLoaded ? (
+          "Sign In"
+        ) : (
+          <Spinner animation="border" variant="light" size="sm" />
+        )}
+      </UIButton>
+    </UIForm>
+  );
 }
 
 export default SignIn;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import AuthContextProvider from "./store/AuthProvider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
       <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
-
