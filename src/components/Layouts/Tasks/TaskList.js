16d30520892da8f98bc33b5390b2cf491003521b
Uju Somto <officialdustman@gmail.com>
2023-11-22 16:49:44 +0100

    created conditional rendering in TaskList.js

--- src/components/Layouts/Tasks/TaskList.js
+++ src/components/Layouts/Tasks/TaskList.js
@@ -4,9 +4,13 @@ import TaskItem from "./TaskItem";
 export default function TaskList({tasks}) {
     return (
         <>
-            {tasks?.data.map((task) => (
-                <TaskItem task={task}/>
-            ))}
+            {tasks? 
+                (tasks?.data.map((task) => (
+                    <TaskItem task={task}/>
+                ))) :
+                
+                <p>No Task Yet!</p>
+        }
         </>
     )
     
