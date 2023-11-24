43fdce74a1e7ebe8d038b8f22eac119490fe0714
Uju Somto <officialdustman@gmail.com>
2023-11-24 18:58:02 +0100

    checking for task

--- src/components/Layouts/Tasks/TaskList.js
+++ src/components/Layouts/Tasks/TaskList.js
@@ -1,13 +1,14 @@
 import TaskItem from "./TaskItem";
+import { useState } from "react";
 
 
 export default function TaskList({tasks}) {
     console.log(tasks);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     return (
         <>
-            {/* <p>No Task Yet!</p> */}
-            if (tasks) {
+            if (isLoaded) {
                 tasks?.data.map((task) => (
                     <TaskItem task={task}/>
                 )) 
