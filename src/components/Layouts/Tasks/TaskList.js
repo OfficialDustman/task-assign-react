7dcbb59e7535d871c6e7a23dd8dd9c62731188e9
Uju Somto <officialdustman@gmail.com>
2023-11-24 19:18:27 +0100

    update task map tasklist.js

--- src/components/Layouts/Tasks/TaskList.js
+++ src/components/Layouts/Tasks/TaskList.js
@@ -1,20 +1,14 @@
 import TaskItem from "./TaskItem";
-import { useState } from "react";
 
 
 export default function TaskList({tasks}) {
     console.log(tasks);
-    const [isLoaded, setIsLoaded] = useState(false);
 
     return (
         <>
-            if (isLoaded) {
-                tasks?.data.map((task) => (
-                    <TaskItem task={task}/>
-                )) 
-            } else {
-                <p>No Task Yet!</p>
-            }
+            {tasks?.data.map((task) => (
+                <TaskItem task={task}/>
+            )) }
         </>
     )
     
