199697e5eabcced15598355e051bb4edb56fae3c
Uju Somto <officialdustman@gmail.com>
2023-11-27 09:58:08 +0100

    updated Navbar.js to dynamically render the navlink

--- src/components/Layouts/NavBar.js
+++ src/components/Layouts/NavBar.js
@@ -3,11 +3,17 @@ import { Navbar, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Navigation = () => {
-  const activeLink =  {
+  const activeLinkStyle = {
     backgroundColor: '#ddd',
-    color: '#333'
-  }
-  
+    color: '#333',
+  };
+
+  const routes = [
+    { path: '/home/task', icon: 'fa-solid fa-house', text: 'Task' },
+    { path: '/home/project', icon: 'fa-solid fa-folder', text: 'Project' },
+    { path: '/home/profile', icon: 'fa-solid fa-user', text: 'Profile' },
+    // Add more routes as needed
+  ];
 
   return (
     <Navbar
@@ -18,74 +24,39 @@ const Navigation = () => {
         alignItems: 'flex-start',
         gap: '2px',
         flex: '1 0 0',
-        alignSelf: 'stretch',
-        justifyContent: 'center', 
-        position: 'fixed', 
+        alignSelf: 'center',
+        justifyContent: 'center',
+        position: 'fixed',
         bottom: 0,
         width: '100%',
         borderRadius: '141px',
         background: '#FFF',
-        boxShadow: '0px 4px 80px 0px rgba(13, 16, 28, 0.20)'
+        boxShadow: '1px 0px 5px 1px rgba(13, 16, 28, 0.20)',
       }}
     >
-      <NavLink
-        to="/home/task"
-        exact
-        className="nav-link"
-        activeClassName={activeLink}
-        style={{
-          display: 'flex',
-          padding: '10px 8px',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: '4px',
-          flex: '1 0 0',
-          alignSelf: 'stretch',
-        }}
-      >
-        <Button variant="light" className={'rounded-pill'}>
-          <FontAwesomeIcon icon="fa-solid fa-house" />
-        </Button>
-      </NavLink>
-      <NavLink
-        to="/home/project"
-        className="nav-link"
-        activeClassName={activeLink}
-        style={{
-          display: 'flex',
-          padding: '10px 8px',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: '4px',
-          flex: '1 0 0',
-          alignSelf: 'stretch',
-        }}
-      >
-        <Button variant="light" className={'rounded-pill'}>
-          <FontAwesomeIcon icon="fa-solid fa-folder" />
-        </Button>
-      </NavLink>
-      <NavLink
-        to="/home/profile"
-        className="nav-link"
-        activeClassName={activeLink}
-        style={{
-          display: 'flex',
-          padding: '10px 8px',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: '4px',
-          flex: '1 0 0',
-          alignSelf: 'stretch',
-        }}
-      >
-        <Button variant="light" className={'rounded-pill'}>
-          <FontAwesomeIcon icon="fa-solid fa-user" />
-        </Button>
-      </NavLink>
+      {routes.map((route, index) => (
+        <NavLink
+          key={index}
+          to={route.path}
+          exact
+          className="nav-link"
+          style={{
+            ...(route.path === window.location.pathname ? activeLinkStyle : {}),
+            display: 'flex',
+            padding: '10px 8px',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '4px',
+            flex: '1 0 0',
+            alignSelf: 'stretch',
+          }}
+        >
+          <Button variant="light" className={'rounded-pill'}>
+            <FontAwesomeIcon icon={route.icon} />
+          </Button>
+        </NavLink>
+      ))}
     </Navbar>
   );
 };
