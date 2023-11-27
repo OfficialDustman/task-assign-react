d015235f463fbe3223bbc3065789774f688eed69
Uju Somto <officialdustman@gmail.com>
2023-11-27 10:53:06 +0100

    updates to styling in  Header.js

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,10 +1,28 @@
 import { Card } from 'react-bootstrap';
 
-export default function Header({userData}) {
+export default function Header({ userData }) {
     return (
-        <header>
-            <Card.Subtitle>Good Morning {userData.name}!</Card.Subtitle>
-            <Card.Title>{new Date().toDateString() }</Card.Title>
+        <header style={{
+            display: 'flex',
+            width: '100%',
+            padding: ' 8px 20px',
+            alignItems: 'center',
+            gap: '1rem',
+        }}>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'flex-start',
+                gap: '4px',
+                flex: '1 0 0',
+                alignSelf: 'stretch',
+            }}>
+                <Card.Subtitle>Good Morning {userData.name}!</Card.Subtitle>
+                <Card.Title>{new Date().toDateString()}</Card.Title>
+            </div>
+
+            <FontAwesomeIcon icon="fa-solid fa-bell" />
         </header>
-    )    
+    )
 }
