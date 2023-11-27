ae6d69e3cc6b7ddd7ab0055c9e78c3321bd7c1c4
Uju Somto <officialdustman@gmail.com>
2023-11-27 11:12:53 +0100

    updated styling in Summary.js and TaskCount.js

--- src/components/Layouts/Summary.js
+++ src/components/Layouts/Summary.js
@@ -4,45 +4,66 @@ import TaskCount from "../Ui/TaskCount"
 import { useState } from 'react';
 
 
-export default function Summary({tasks, onFilteredTasksChange, onDateChange}) {
+export default function Summary({ tasks, onFilteredTasksChange, onDateChange }) {
     const [datefilteredTasks, setDateFilteredTasks] = useState([...tasks]);
-  
+
     const handleDateChange = (newDate) => {
-      // Filter tasks based on the selected date and update the state
-      const newFilteredTasks = tasks?.filter(task => {
-        const taskDate = new Date(task.start_date); // Assuming each task has a 'date' property
-        return taskDate.toDateString() === newDate.toDateString();
-      });
-      setDateFilteredTasks(newFilteredTasks);
-      onFilteredTasksChange(newFilteredTasks)
-      onDateChange(newDate)
+        // Filter tasks based on the selected date and update the state
+        const newFilteredTasks = tasks?.filter(task => {
+            const taskDate = new Date(task.start_date); // Assuming each task has a 'date' property
+            return taskDate.toDateString() === newDate.toDateString();
+        });
+        setDateFilteredTasks(newFilteredTasks);
+        onFilteredTasksChange(newFilteredTasks)
+        onDateChange(newDate)
     };
 
     // Count all tasks and completed tasks
     const allTasksCount = datefilteredTasks.length;
     const completedTasksCount = datefilteredTasks.filter(task => task.status === 'completed').length;
 
+    const taskCountStyle = {
+        display: 'flex',
+        padding: '12px',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        gap: '0.5rem',
+        flex: '1 0 0'
+    }
 
     return (
-        <Card style={{ 
-            display: 'flex', 
-            lexDirection: 'column', 
-            padding: '20px', 
-            marginTop: '20px' 
+        <Card style={{
+            display: 'flex',
+            flexDirection: 'column',
+            padding: '20px',
+            marginTop: '20px'
         }}>
-            <Card.Header>
+            <Card.Header style={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: '1rem',
+                alignSelf: 'stretch',
+            }}>
                 <Card.Title>Summary</Card.Title>
-                <DateFilter onDateChange={handleDateChange}/>
+                <DateFilter onDateChange={handleDateChange} />
             </Card.Header>
 
-            <CardGroup style={{ marginTop: '20px' }}>
-                <TaskCount 
-                    state={'Assigned'} 
+            <CardGroup style={{
+                marginTop: '20px',
+                display: 'flex',
+                alignItems: 'flex-start',
+                gap: '0.5rem',
+                alignSelf: 'stretch'
+            }}>
+                <TaskCount
+                    state={'Assigned'}
                     count={allTasksCount}
+                    styles={taskCountStyle}
                 />
-                <TaskCount 
-                    state={'Completed'} 
+                <TaskCount
+                    state={'Completed'}
                     count={completedTasksCount}
+                    styles={taskCountStyle}
                 />
             </CardGroup>
         </Card>
--- src/components/Ui/TaskCount.js
+++ src/components/Ui/TaskCount.js
@@ -1,11 +1,19 @@
 import { Card } from "react-bootstrap"
 
-export default function TaskCount({state, count}) {
+export default function TaskCount({state, count, styles}) {
     return (
-        <Card>
+        <Card style={styles}>
             <Card.Subtitle>{state} Tasks</Card.Subtitle>
 
-            <Card.Title>{count}</Card.Title>
+            <Card.Title style={{
+                fontSize: '24px',
+                fontStyle: 'normal',
+                fontWeight: 500,
+                lineHeight: '130%',
+                letterSpacing: '-0.48px'
+            }}>
+                {count}
+            </Card.Title>
         </Card>
     )
 }
