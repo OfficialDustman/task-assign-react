0d66e25c145d2947051459b6ab107f98e0a36843
Uju Somto <officialdustman@gmail.com>
2023-11-27 08:34:11 +0100

    updated tasklist to conditionally render task

--- src/components/Layouts/Tasks/TaskList.js
+++ src/components/Layouts/Tasks/TaskList.js
@@ -6,9 +6,13 @@ export default function TaskList({tasks}) {
 
     return (
         <>
-            {tasks?.map((task) => (
-                <TaskItem task={task}/>
-            )) }
+            {tasks ? 
+                (tasks?.map((task) => (
+                    <TaskItem task={task}/>
+                )) ) :
+
+                (<p>No Task Yet</p>)
+            }
         </>
     )
     
