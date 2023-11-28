af7347b9b1d28a41b095629793c604f068693dd7
Uju Somto <officialdustman@gmail.com>
2023-11-28 21:17:33 +0100

    updatecd styling in sidebar

--- src/components/Layouts/Projects/SideBar.js
+++ src/components/Layouts/Projects/SideBar.js
@@ -4,13 +4,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ProjectAccordion from "./ProjectAccordion";
 
 
-const Sidebar = ({projects}) => { 
+const Sidebar = ({ projects }) => {
   return (
-    <Navbar expand="lg" className="bg-light">
-      <Navbar.Toggle aria-controls="basic-navbar-nav">
+    <Navbar
+      expand="lg"
+      className="bg-light"
+      style={{
+        width: '30%',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'flex-start',
+        flexDirection: 'column',
+        gap: '1rem'
+      }}
+    >
+      {/* <Navbar.Toggle aria-controls="basic-navbar-nav">
         <FontAwesomeIcon icon="fa-solid fa-bars" />
-      </Navbar.Toggle>
-      <Navbar.Collapse id="basic-navbar-nav">
+      </Navbar.Toggle> */}
+      <Navbar.Collapse
+        id="basic-navbar-nav"
+        style={{
+          flexBasis: 0,
+          flexGrow: 0,
+        }}
+      >
         <Nav className="flex-column">
           <ProjectAccordion projects={projects} />
         </Nav>
