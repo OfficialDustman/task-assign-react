25d393914062ea8f5bb2c833aab361c59dc6091a
Uju Somto <officialdustman@gmail.com>
2023-11-28 19:13:50 +0100

    update to commit logs in ProjectAccordion.js

--- src/components/Layouts/Projects/ProjectAccordion.js
+++ src/components/Layouts/Projects/ProjectAccordion.js
@@ -5,27 +5,30 @@ const ProjectAccordion = ({ projects }) => {
 
   console.log(projects);
 
+  projects.map(project => {
+    console.log(project, project.project_id, project.project_name)
+  })
+
   return (
     <Accordion>
-      {projects.map(project => (
-        {project}
-        // <Card key={project.project_id}>
-        //   <Card.Header>
-        //     <Accordion.Toggle as={Button} variant="link" eventKey={project.project_id.toString()}>
-        //       {project.project_name}
-        //     </Accordion.Toggle>
-        //   </Card.Header>
-        //   <Accordion.Collapse eventKey={project.project_id.toString()}>
-        //     <Card.Body>
-        //       <ul>
-        //         {project.tasks.map(task => (
-        //           <li key={task.task_id}>{task.task_name}</li>
-        //         ))}
-        //       </ul>
-        //     </Card.Body>
-        //   </Accordion.Collapse>
-        // </Card>
-      ))}
+      {/* {projects.map(project => (
+        <Card key={project.project_id}>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey={project.project_id.toString()}>
+              {project.project_name}
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey={project.project_id.toString()}>
+            <Card.Body>
+              <ul>
+                {project.tasks.map(task => (
+                  <li key={task.task_id}>{task.task_name}</li>
+                ))}
+              </ul>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      ))} */}
     </Accordion>
   );
 };
