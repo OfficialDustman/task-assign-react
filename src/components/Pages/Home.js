fee364a8260eff5ebfb4825fa5676392c8a5e750
Uju Somto <officialdustman@gmail.com>
2023-12-15 10:54:59 +0100

    removed conditional routing in Home.js

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,30 +1,14 @@
 import { useLocation, useNavigate, Outlet } from "react-router-dom";
-import NavBar from '../Layouts/NavBar';
-import AuthContext from "../../store/auth-context";
-import { useState, useEffect, useContext } from "react";
+import NavBar from '../Layouts/NavBar'
+import { useEffect } from "react";
 
 function Home() {
-
-    const [isLogged, setIsLogged] = useState(false)
-    const { userData } = useContext(AuthContext)
-    console.log(userData);
 
     const navigate = useNavigate(),
         location = useLocation();
 
     useEffect(() => {
-        if (userData.length < 0) {
-            setIsLogged(false)
-        } else if (userData) {
-            setIsLogged(true)
-        }
-    }, [userData]);
-
-    useEffect(() => {
-        if (!isLogged && location.pathname === '/home') {
-            navigate("/");
-        }
-        else if (isLogged && location.pathname === '/home') {
+        if (location.pathname === '/home') {
             navigate("/home/task");
         }
     }, [location, navigate]);
@@ -39,13 +23,8 @@ function Home() {
                 background: 'linear-gradient(180deg, #FFF 0%, #DFE4F1 100%)'
             }}
         >
-            {isLogged &&
-                <>
-                    <Outlet />
-                    <NavBar />
-                </>
-            }
-
+            <Outlet />
+            <NavBar />
         </div>
     )
 }
