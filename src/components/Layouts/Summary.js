80b62462b4ea16827883330276239182e896900d
Uju Somto <officialdustman@gmail.com>
2023-12-28 12:08:13 +0100

    Date commit log in Summary.js

--- src/components/Layouts/Summary.js
+++ src/components/Layouts/Summary.js
@@ -7,20 +7,22 @@ import { useState } from 'react';
 export default function Summary({ tasks, onFilteredTasksChange, onDateChange }) {
     const [datefilteredTasks, setDateFilteredTasks] = useState([...tasks]);
 
+    // Count all tasks and completed tasks
+    const allTasksCount = datefilteredTasks.length;
+    const completedTasksCount = datefilteredTasks.filter(task => task.status === 'completed').length;
+
     const handleDateChange = (newDate) => {
         // Filter tasks based on the selected date and update the state
         const newFilteredTasks = tasks?.filter(task => {
             const taskDate = new Date(task.start_date); // Assuming each task has a 'date' property
+            console.log(taskDate, newDate);
+            console.log(taskDate.toDateString(), newDate.toDateString());
             return taskDate.toDateString() === newDate.toDateString();
         });
         setDateFilteredTasks(newFilteredTasks);
         onFilteredTasksChange(newFilteredTasks)
         onDateChange(newDate)
     };
-
-    // Count all tasks and completed tasks
-    const allTasksCount = datefilteredTasks.length;
-    const completedTasksCount = datefilteredTasks.filter(task => task.status === 'completed').length;
 
     const taskCountStyle = {
         display: 'flex',
